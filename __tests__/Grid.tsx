7623a13aceed56015e08656539bbca453be8b1d4
Christopher Brown <chbphone55@gmail.com>
2019-05-22 22:41:45 -0600

    trying to get the tests to work

--- __tests__/Grid.tsx
+++ __tests__/Grid.tsx
@@ -2,22 +2,33 @@ import React from 'react'
 import { Grid } from '../components/Grid'
 
 import { render, cleanup } from 'react-testing-library'
+import { wait } from 'dom-testing-library'
 import 'jest-dom/extend-expect'
 
-test('should render', () => {
-  const { debug } = render(
+afterEach(cleanup)
+
+test('should render', async () => {
+  const { debug, getByText } = render(
     <Grid
       items={[1, 2, 3, 4, 5, 6].map(x => ({
         value: x,
-        width: 100,
-        height: 100
+        width: 32,
+        height: 32
       }))}
       keys={(item: any) => item.value}
-      renderer={({ data, ...props }) => <div {...props}>{data.value}</div>}
+      renderer={({ data, style }) => <div style={style}>{data.value}</div>}
       wrapper="section"
+      style={{
+        width: '5rem'
+      }}
     />
   )
 
+  await wait(() => {
+    if (!getByText('1').style.left) {
+      throw new Error('left missing')
+    }
+  })
   debug()
 })
 
